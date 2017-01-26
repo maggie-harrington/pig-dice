f8a5900925eea905ea808289285d3860d3b13d0e
Maggie Harrington <maggie.harrington@gmail.com>
2017-01-25 16:08:16 -0800

    replace alert for rolling 1 with text on page

--- js/scripts.js
+++ js/scripts.js
@@ -18,14 +18,16 @@ $(document).ready(function(){
     event.preventDefault();
     $("#current-roll-player1").empty();
     $("#turn-total-player1").empty();
+    $("#rolled1-player2").hide();
     var result1 = dieRoll();
     $("#current-roll-player1").append(result1);
     if (result1 === 1) {
       turnTotal1 = 0;
-      alert ("Player 2's turn!");
       $("#turn-total-player1").empty();
       $(".hide1").hide();
       $(".hide2").show();
+      $("#rolled1-player1").show();
+      $("#rolled1-player2").hide();
     } else {
       turnTotal1 += result1;
       $("#turn-total-player1").append(turnTotal1);
@@ -36,14 +38,16 @@ $(document).ready(function(){
     event.preventDefault();
     $("#current-roll-player2").empty();
     $("#turn-total-player2").empty();
+    $("#rolled1-player1").hide();
     var result2 = dieRoll();
     $("#current-roll-player2").append(result2);
     if (result2 === 1) {
       turnTotal2 = 0;
-      alert ("Player 1's turn!");
       $("#turn-total-player2").empty();
       $(".hide2").hide();
       $(".hide1").show();
+      $("#rolled1-player2").show();
+      $("#rolled1-player1").hide();
     } else {
       turnTotal2 += result2;
       $("#turn-total-player2").append(turnTotal2);
