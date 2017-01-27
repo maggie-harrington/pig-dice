e23b9a87f2ec7bd01a509dceac862efadfea3dc5
Maggie Harrington <maggie.harrington@gmail.com>
2017-01-26 18:19:48 -0800

    attempt to consolidate js

--- js/scripts-alt.js
+++ js/scripts-alt.js
@@ -8,6 +8,35 @@ function dieRoll() {
   return Math.floor(Math.random()*6 + 1);
 }
 
+function newRoll() {
+  $("#current-roll-player1").empty();
+  $("#turn-total-player1").empty();
+  $("#rolled1-player1").hide();
+  $("#current-roll-player2").empty();
+  $("#turn-total-player2").empty();
+  $("#rolled1-player2").hide();
+}
+function newTurn() {
+  turnTotal1 = 0;
+  $(".hide1").hide();
+  turnTotal2 = 0;
+  $(".hide2").hide();
+  newRoll();
+}
+function updateGameScore() {
+  $("#game-score-player1").empty();
+  $("#game-score-player2").empty();
+  newTurn();
+}
+
+function winGame() {
+  gameScore1 = 0;
+  gameScore2 = 0;
+  $("#new-game").show();
+  updateGameScore();
+}
+
+
 
 
 
@@ -16,89 +45,63 @@ function dieRoll() {
 $(document).ready(function(){
   $("form#roll-die-player1").submit(function(event){
     event.preventDefault();
-    $("#current-roll-player1").empty();
-    $("#turn-total-player1").empty();
-    $("#rolled1-player2").hide();
+    newRoll();
     var result1 = dieRoll();
     $("#current-roll-player1").append(result1);
     if (result1 === 1) {
-      turnTotal1 = 0;
-      $("#turn-total-player1").empty();
-      $("#current-roll-player1").empty();
-      $(".hide1").hide();
+      newTurn();
       $(".hide2").show();
       $("#rolled1-player1").show();
-      $("#rolled1-player2").hide();
     } else {
       turnTotal1 += result1;
       $("#turn-total-player1").append(turnTotal1);
       if (gameScore1 + turnTotal1 >= 30) {
+        updateGameScore();
         gameScore1 += turnTotal1;
-        $("#game-score-player1").empty();
         $("#game-score-player1").append(gameScore1);
         $("#win-player1").show();
-        $(".hide2").hide();
-        $(".hide1").hide();
-        $("#new-game").show();
-        gameScore1 = 0;
-        gameScore2 = 0;
+        winGame();
       }
     }
   });
 
   $("form#roll-die-player2").submit(function(event){
     event.preventDefault();
-    $("#current-roll-player2").empty();
-    $("#turn-total-player2").empty();
-    $("#rolled1-player1").hide();
+    newRoll();
     var result2 = dieRoll();
     $("#current-roll-player2").append(result2);
     if (result2 === 1) {
-      turnTotal2 = 0;
-      $("#turn-total-player2").empty();
-      $("#current-roll-player2").empty();
-      $(".hide2").hide();
+      newTurn();
       $(".hide1").show();
       $("#rolled1-player2").show();
-      $("#rolled1-player1").hide();
     } else {
       turnTotal2 += result2;
       $("#turn-total-player2").append(turnTotal2);
       if (gameScore2 + turnTotal2 >= 30) {
+        updateGameScore();
         gameScore2 += turnTotal2;
-        $("#game-score-player2").empty();
         $("#game-score-player2").append(gameScore2);
         $("#win-player2").show();
-        $(".hide1").hide();
-        $(".hide2").hide();
-        $("#new-game").show();
-        gameScore2 = 0;
-        gameScore1 = 0;
+        winGame();
       }
     }
   });
 
   $("form#hold-player1").submit(function(event) {
     event.preventDefault();
-    $("#game-score-player1").empty();
-    $("#turn-total-player1").empty();
-    $("#current-roll-player1").empty();
+    updateGameScore();
     gameScore1 += turnTotal1;
     $("#game-score-player1").append(gameScore1);
     turnTotal1 = 0;
-    $(".hide1").hide();
     $(".hide2").show();
   });
 
   $("form#hold-player2").submit(function(event) {
     event.preventDefault();
-    $("#game-score-player2").empty();
-    $("#turn-total-player2").empty();
-    $("#current-roll-player2").empty();
+    updateGameScore();
     gameScore2 += turnTotal2;
     $("#game-score-player2").append(gameScore2);
     turnTotal2 = 0;
-    $(".hide2").hide();
     $(".hide1").show();
   });
 });
